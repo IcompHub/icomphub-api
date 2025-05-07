3947f85535c3d9b33a54194ec5077586d2100c05
Nelson Neto <82171194+nelsul@users.noreply.github.com>
2025-05-07 05:31:27 -0400

    chore: add support to `.env` file

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,32 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error while loading .env file")
+	}
+
+	apiPort := os.Getenv("INTERNAL_API_PORT")
+
+	if apiPort == "" {
+		log.Fatal("Error while loading INTERNAL_API_PORT from .env file")
+	}
+
 	server := gin.Default()
 
 	server.GET("/", func(ctx *gin.Context) {
 		ctx.JSON(200, gin.H{
-			"message": "IcompHub API is running!",
+			"message": "IcompHub API is running! hehe",
 		})
 	})
 
-	server.Run(":8000")
+	server.Run(":" + apiPort)
 }
